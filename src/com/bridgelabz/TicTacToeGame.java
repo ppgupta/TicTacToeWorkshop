b91fa0d63fe04062a2839ccc12429dcd652d71bb
ppgupta <premprakashnayak69@gmail.com>
2020-10-16 09:42:32 +0530

    create empty space

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -1,8 +1,19 @@
 package com.bridgelabz;
 
 public class TicTacToeGame {
+	char []board;
 	
+	//UC1
+			private char[] assignEmptySpace() {
+				this.board = new char[10];
+				for (int index = 1; index < 10; index++) {
+					board[index] = ' ';
+				}
+				return board;
+			}
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tac Toe Game");
+		TicTacToeGame ticTacToeGame = new TicTacToeGame();
+		char[] ticTacToeBoard = ticTacToeGame.assignEmptySpace();
 	}
 }
